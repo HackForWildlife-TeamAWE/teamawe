d7380928f979756dc02ee1d78ddfd3ce444f9111
manny <elnels@gmail.com>
2015-11-14 21:58:14 -0600

    CORS

--- routes/api/locations.js
+++ routes/api/locations.js
@@ -7,6 +7,8 @@ var async = require('async'),
 	exports = module.exports = function(req, res) {
 		keystone.list('Location').model.find().exec(function(err,results){
 			if (err) return res.status(500).json({ success: false, err: err });
+			res.setHeader('Access-Control-Allow-Origin', '*');
+			//res.writeHead(200, { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" });
 			return res.json({success:true, data:results});
 		})
 	};
